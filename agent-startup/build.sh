1424ad9713fedcd28e4960321d476d2c78a11fe1
zinklandi <zinklandi@users.noreply.github.com>
2019-08-18 11:28:31 +0200

    update build script

--- agent-startup/build.sh
+++ agent-startup/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # build a exe with a window
-docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller agent_startup.py --onefile"
+docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller agent_startup.py --onefile"
 mv ./dist/agent_startup.exe ./dist/agent_startup_window.exe
 
 # build a exe without a window
-docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller agent_startup.py --onefile --windowed"
+docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller agent_startup.py --onefile --windowed"
